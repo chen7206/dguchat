898c7d9c9883b86935e08eda2a3ac1ddea515927
rudebono <rudebono@gmail.com>
2017-06-03 15:47:47 +0900

    Update : add main function

--- public/dguchat.js
+++ public/dguchat.js
@@ -1,11 +1,19 @@
-// Get index <video> tag.
-var videoArea = document.querySelector("video");
+// main function
+function main() {
+	// Get index <video> tag.
+	var videoArea = document.querySelector("video");
 
-// get User Media fucntion on diffrent browsers.
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+	// get User Media fucntion on diffrent browsers.
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-// set constraints.
-var constraints = {video: true, audio: true}
+	// set constraints.
+	var constraints = {video: true, audio: true}
+
+	// start getUderMedia
+	navigator.getUserMedia(constraints, onSuccess, onError);
+
+}// END - main funciton
+
 
 // success function
 function onSuccess(stream) {
@@ -15,10 +23,11 @@ function onSuccess(stream) {
 	// set video src and play.
 	videoArea.src = window.URL.createObjectURL(stream);
 	videoArea.play();
-}
+}// END - onSuccess function
 
-// error funciton
+
+// error function
 function onError(error) {
-	console.log("ERROR : getUserMedia : ", error);
-}
 
+	console.log("ERROR : getUserMedia : ", error);
+}// END - error function
