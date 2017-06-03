6673d833c8c1757ee602a385d7c50e99d12198c2
rudebono <rudebono@gmail.com>
2017-06-04 02:33:29 +0900

    Updatae : heroku app must port on heroku defulat

--- server.js
+++ server.js
@@ -16,6 +16,4 @@ app.get('/', function(req, res) {
 });
 
 // listen clients.
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port : ', app.get('port'));
-});
+app.listen(app.get('port'));
