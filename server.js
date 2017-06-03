535b3118d0f23ac7e9e09f300a7fc9880b508e38
rudebono <rudebono@gmail.com>
2017-06-03 14:39:57 +0900

    Update : remove listen fucntion.

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// make express instance
 var express = require('express');
 var app = express();
 
+// set port by heroku's dafult ports.
 app.set('port', (process.env.PORT));
 
+// set public folder (CSS)
 app.use(express.static(__dirname + '/public'));
 
+// set views folder (ejs)
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// request and response function
 app.get('/', function(request, response) {
-  response.render('index');
+	// request / then response index.ejs file.
+	response.render('index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
+// listen clients.
+//app.listen(app.get('port'), function() {
+//  console.log('Node app is running on port', app.get('port'));
+//});
