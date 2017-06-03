ff3b3a1954592b104549e2e9fff0aee51b1d2257
rudebono <rudebono@gmail.com>
2017-06-03 14:44:25 +0900

    Update : make listen function on server.js

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('/', function(request, response) {
 });
 
 // listen clients.
-//app.listen(app.get('port'), function() {
-//  console.log('Node app is running on port', app.get('port'));
-//});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
