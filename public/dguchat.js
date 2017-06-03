0f3234fb8c3470c7dd55a7a65d5f0c22a27dadc9
rudebono <rudebono@gmail.com>
2017-06-04 02:38:22 +0900

    Update : remove main

--- public/dguchat.js
+++ public/dguchat.js
@@ -1,43 +1,31 @@
 /*
 	define functions
 */
-window.onload = main;
+// Get index <video> tag.
+var videoArea = document.querySelector("#local-video");
 
-function main() {
-	
-	setMedia();
-}// END - main funciton
+// get User Media fucntion on diffrent browsers.
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
+// set constraints.
+var constraints = {video: true, audio: true};
 
-// setMedia function
-function setMedia() {
-
-	// Get index <video> tag.
-	var videoArea = document.querySelector("#local-video");
-
-	// get User Media fucntion on diffrent browsers.
-	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-	// set constraints.
-	var constraints = {video: true, audio: true};
-
-	// start getUderMedia
-	navigator.getUserMedia(constraints, onSuccess, onError);
+// start getUderMedia
+navigator.getUserMedia(constraints, onSuccess, onError);
 
 	
-	// success function
-	function onSuccess(stream) {
+// success function
+function onSuccess(stream) {
 
-		console.log("SUCCESS : getUserMedia.");
+	console.log("SUCCESS : getUserMedia.");
 
-		// set video src and play.
-		videoArea.src = window.URL.createObjectURL(stream);
-		videoArea.play();
-	}// END - onSuccess function
+	// set video src and play.
+	videoArea.src = window.URL.createObjectURL(stream);
+	videoArea.play();
+}// END - onSuccess function
 
-	// error function
-	function onError(error) {
+// error function
+function onError(error) {
 
-		console.log("ERROR : getUserMedia : ", error);
-	}// END - error function
-}// END - setMedia function
+	console.log("ERROR : getUserMedia : ", error);
+}// END - error function
