6b75a8085484750f6904ea5d6cfee214417b7d22
rudebono <rudebono@gmail.com>
2017-06-06 01:43:07 +0900

    Update : test see the all room id

--- js/dguchat.js
+++ js/dguchat.js
@@ -35,3 +35,34 @@ document.getElementById('btn-open-or-join-room').onclick = function() {
 	this.disabled = true;
 	connection.openOrJoin(roomid.value || 'predefined-roomid');
 };
+
+
+
+var publicRoomsDiv = document.getElementById('public-rooms');
+            (function looper() {
+                // connection.getPublicModerators(startsWith, callback)
+                connection.getPublicModerators(function(array) {
+                    publicRoomsDiv.innerHTML = '';
+                    array.forEach(function(moderator) {
+                        // moderator.userid
+                        // moderator.extra
+                        if(moderator.userid == connection.userid) return; // if owner himself
+                        var li = document.createElement('li');
+                        li.innerHTML = '<b>' + moderator.userid + '</b>&nbsp;&nbsp;';
+                        var button = document.createElement('button');
+                        button.id = moderator.userid;
+                        button.onclick = function() {
+                            this.disabled = true;
+                            connection.join(this.id);
+                        };
+                        button.innerHTML = 'Join this room';
+                        li.appendChild(button);
+                        if(moderator.userid == connection.sessionid) {
+                            // if already connected with same moderator
+                            button.disabled = true;
+                        }
+                        publicRoomsDiv.insertBefore(li, publicRoomsDiv.firstChild);
+                    });
+                    setTimeout(looper, 3000);
+                });
+            })();
