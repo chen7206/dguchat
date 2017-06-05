32b9ca643a59a1d2a3bfcb68ea1f4d13b976adfe
rudebono <rudebono@gmail.com>
2017-06-05 19:10:21 +0900

    Update : basic rtc with RTCMulticonnection

---
+++ dguchat.js
@@ -0,0 +1,39 @@
+var connection = new RTCMultiConnection();
+
+connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+
+connection.session = {
+	audio:true,
+	video:true
+};
+
+connection.sdpConstraints.mandatory = {
+	OfferToReceiveAudio : true,
+	OfferToReceiveVideo : true
+};
+
+
+var localVideoContainer = document.getElementById('local-videos-container');
+var remoteVideoContainer = document.getElementById('remote-videos-container');
+
+connection.onstream = function(event) {
+
+	var video = event.mediaElement;
+
+	if(event.type == 'local') {
+		localVideoContainer.appendChild(video);
+	}
+
+	if(event.type == 'remote') {
+		remoteVideoContainer.appendChild(video);
+	}
+};
+
+var roomid = document.getElementById('txt-roomid');
+//roomid.value = connection.token();
+roomid.value = Math.round(Math.random() * 100);
+
+document.getElementById('btn-open-or-join-room').onclick = function() {
+	this.disabled = true;
+	connection.openOrJoin(roomid.value || 'predefined-roomid');
+};
