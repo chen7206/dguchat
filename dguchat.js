06c296079ea3470cfb9bd4269a21b6d7bac3a613
rudebono <rudebono@gmail.com>
2017-06-05 21:16:27 +0900

    Update : socketio rudebono.herokuapp.com

--- dguchat.js
+++ dguchat.js
@@ -1,6 +1,6 @@
 var connection = new RTCMultiConnection();
 
-connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+connection.socketURL = 'https://rudebono.herokuapp.com:443/';
 
 connection.session = {
 	audio:true,
