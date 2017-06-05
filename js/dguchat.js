c3b790d3e1aeb9ef88faab523078a7ce2dcc9139
rudebono <rudebono@gmail.com>
2017-06-06 05:21:19 +0900

    ERROR in getPublicModerators

--- js/dguchat.js
+++ js/dguchat.js
@@ -13,53 +13,46 @@ connection.sdpConstraints.mandatory = {
 };
 
 
-var localVideoContainer = document.getElementById('local-videos-container');
-var remoteVideoContainer = document.getElementById('remote-videos-container');
+var videoContainer = document.getElementById('videos-container');
 
 connection.onstream = function(event) {
 
-	var video = event.mediaElement;
-
-	console.log(event.type);
-	localVideoContainer.appendChild(video);
-/*
-	if(event.type == 'local') {
-		console.log("local!");
-		localVideoContainer.appendChild(video);
-		//video.play();
-	}
-
-	if(event.type == 'remote') {
-		console.log("remote!");
-		remoteVideoContainer.appendChild(video);
-		//video.play();
-	}
-
-*/
+	video = event.mediaElement;
+	videoContainer.appendChild(video);
+	video.play();
 };
 
 
+
 var roomid = document.getElementById('text-roomid');
+
+
 document.getElementById('btn-open-or-join-room').onclick = function() {
 	this.disabled = true;
 
-	var chkPublic = document.getElementById('chk_public_room').checked;
-	var isPublicModerator = false;
-	if(chkPublic == true)
-		var isPublicModerator = true;
-
-        connection.open(roomid.value || 'predefined-roomid', isPublicModerator);
+	//var chkPublic = document.getElementById('chk_public_room').checked;
+	//var isPublicModerator = false;
+	//if(chkPublic === true)
+	//	isPublicModerator = true;
+        
+	connection.openOrJoin(roomid.value);
+	//connection.open(roomid.value, isPublicModerator);
+	
 	displayNone();
 
 };
 
 
-
+// ERROR : no connection
+/*
 var publicRoomsDiv = document.getElementById('public-rooms');
 	(function looper() {
+		console.log("looper");
 		// connection.getPublicModerators(startsWith, callback)
+
 		connection.getPublicModerators(function(array) {
 			publicRoomsDiv.innerHTML = '';
+			
 			array.forEach(function(moderator) {
 				// moderator.userid
 				// moderator.extra
@@ -87,16 +80,17 @@ var publicRoomsDiv = document.getElementById('public-rooms');
 
 				publicRoomsDiv.insertBefore(li, publicRoomsDiv.firstChild);
 			});
-			setTimeout(looper, 4000);
+			
+			setTimeout(looper, 3000);
 		});
 	})();
-
+*/
 
 // 연결이 성공된 후 실행 되는 함수이다.
 // 채팅방 입력, 체크박스, 시작버튼, 채팅방 리스트가 사라진다.
 function displayNone() {
 	document.getElementById("text-roomid").style.display = "none";
-	document.getElementById("public_room").style.display = "none";
+	//document.getElementById("public_room").style.display = "none";
 	document.getElementById("btn-open-or-join-room").style.display = "none";
-	document.getElementById("list-room").style.display = "none";
+	//document.getElementById("list-room").style.display = "none";
 }
